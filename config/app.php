8ef8d444a7fbf9dc682716c4c9a37e5936f92a8e
Esa Khan <esa.khan28@gmail.com>
2020-06-21 14:56:15 +0500

    Feature - Notification to driver on order creation

--- config/app.php
+++ config/app.php
@@ -170,6 +170,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Laravel IDE helper
+//        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider,
 
     ],
 
