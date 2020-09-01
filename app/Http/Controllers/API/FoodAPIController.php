5a7ddab606dd29d80d426b32202d6ff09dcdde6a
Esa Khan <esa.khan@mail.mcgill.ca>
2020-09-01 19:11:28 -0400

    Shorten Food Request

--- app/Http/Controllers/API/FoodAPIController.php
+++ app/Http/Controllers/API/FoodAPIController.php
@@ -76,16 +76,18 @@ class FoodAPIController extends Controller
 //            $this->foodRepository->orderBy('area');
 
             if (isset($request['short'])){
-                $foods = $this->foodRepository->createQueryBuilder()->select('id', 'name', 'price', 'discount_price', 'description', 'ingredients', 'weight',
-                    'featured', 'deliverable', 'restaurant_id', 'category_id', 'image_url', 'commission',
-                    'has_media', 'media')->getQuery()->getResult();
-//                $foods = $this->foodRepository->all('id', 'name', 'price', 'discount_price', 'description', 'ingredients', 'weight',
+//                $foods = $this->foodRepository->createQueryBuilder()->select('id', 'name', 'price', 'discount_price', 'description', 'ingredients', 'weight',
 //                    'featured', 'deliverable', 'restaurant_id', 'category_id', 'image_url', 'commission',
-//                    'has_media', 'media');
+//                    'has_media', 'media')->getQuery()->getResult();
+
+                $foods = $this->foodRepository->all('id', 'name', 'price', 'discount_price', 'description', 'ingredients', 'weight',
+                    'featured', 'deliverable', 'restaurant_id', 'category_id', 'image_url', 'commission',
+                    'has_media', 'media');
+
 //                $foods = $this->foodRepository->only('id', 'name', 'price', 'discount_price', 'description', 'ingredients', 'weight',
 //                    'featured', 'deliverable', 'restaurant_id', 'category_id', 'image_url', 'commission',
 //                    'has_media', 'media');
-//
+
 //                $itemsInRange = array();
 //                foreach ($foods->toArray() as $currFood){
 //                   unset($currFood['unit']);
