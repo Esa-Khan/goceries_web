4854951fd9bf0f0751992b5b371dde8b539c2d51
Esa Khan <esa.khan@mail.mcgill.ca>
2020-09-01 19:43:09 -0400

    Shorten Food Request

--- app/Http/Controllers/API/FoodAPIController.php
+++ app/Http/Controllers/API/FoodAPIController.php
@@ -80,7 +80,9 @@ class FoodAPIController extends Controller
 //                $foods = $this->foodRepository->only(['id', 'name', 'price', 'discount_price', 'description', 'ingredients', 'weight',
 //                    'featured', 'deliverable', 'restaurant_id', 'category_id', 'image_url', 'commission',
 //                    'has_media', 'media']);
-                $foods = $this->foodRepository->all(['id', 'name']);
+                $foods = $this->foodRepository->all(['id', 'name', 'price', 'discount_price', 'description', 'ingredients', 'weight',
+                    'featured', 'deliverable', 'restaurant_id', 'category_id', 'image_url', 'commission',
+                    'has_media', 'media']);
                 echo "-----------Got all foods-----------";
 //                $collection = collect($foods->toArray());
 //                $filtered = $collection->only(['id', 'name', 'price', 'discount_price', 'description', 'ingredients', 'weight',
