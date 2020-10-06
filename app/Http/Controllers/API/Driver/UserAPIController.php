466aa621787a21cb9f6809a6e80be2e954caae4f
Esa Khan <esa.khan28@gmail.com>
2020-10-06 02:33:52 -0400

    Returning Driver work_hours

--- app/Http/Controllers/API/Driver/UserAPIController.php
+++ app/Http/Controllers/API/Driver/UserAPIController.php
@@ -61,7 +61,6 @@ class UserAPIController extends Controller
                 $user->device_token = $request->input('device_token', '');
                 $user->save();
                 if ($user->isDriver) {
-                    echo Driver::select('work_hours')->where('user_id', $user->id)->get()->toArray();
                     echo Driver::select('work_hours')->where('user_id', $user->id)->get()->toArray()['work_hours'];
                     echo getArrayColumn(Driver::select('work_hours')->where('user_id', $user->id)->get()->toArray());
                     $user['work_hours'] = Driver::select('work_hours')->where('user_id', $user->id)->get()->toArray()['work_hours'];
