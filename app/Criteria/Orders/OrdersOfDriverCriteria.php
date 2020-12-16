ae0e34ff7f0d372d64bd2c8537679956e3161ab7
Esa Khan <esa.khan28@gmail.com>
2020-12-16 03:56:24 -0500

    Manager integration and icon update

--- app/Criteria/Orders/OrdersOfDriverCriteria.php
+++ app/Criteria/Orders/OrdersOfDriverCriteria.php
@@ -42,12 +42,69 @@ class OrdersOfDriverCriteria implements CriteriaInterface
      */
     public function apply($model, RepositoryInterface $repository)
     {
-        return $model->where([
-                        ['orders.driver_id', '=', $this->request->get('driver_id')],
-                        ['orders.order_status_id', '<', 5]
-                    ])
-                        ->orWhere('orders.driver_id', '1')
-                        ->select('orders.*');
+
+        $isWorking = false;
+        $isManager = DB::table('users')
+                            ->where('users.id', $this->request->get('driver_id'))
+                            ->pluck('users.isManager')
+                            ->first();
+        $work_hours = DB::table('drivers')
+                        ->where('drivers.user_id', $this->request->get('driver_id'))
+                        ->pluck('drivers.work_hours', 'drivers.store_ids')
+                        ->first();
+
+
+        if ($work_hours == '24/7') {
+            $isWorking = true;
+        } else {
+            $current_time = date("H:i:s");
+            $work_hours = explode('|', $work_hours);
+            $start_time = date('H:i:s',strtotime($work_hours[0]));
+            $end_time = date('H:i:s',strtotime($work_hours[1]));
+
+            if ($current_time >= $start_time && $current_time <= $end_time) {
+                $isWorking = true;
+            }
+        }
+        if ($isWorking) {
+            if ($isManager) {
+                return $model->where('orders.order_status_id', '<', 5)
+                                ->orderBy('orders.order_status_id', 'desc')
+                                ->orderBy('orders.id', 'asc')
+                                ->select('orders.*');
+
+            } else {
+                return $model->where([
+                                    ['orders.driver_id', '=', $this->request->get('driver_id')],
+                                    ['orders.order_status_id', '<', 5]
+                                ])
+                                ->orWhere('orders.driver_id', '1')
+                                ->orderBy('orders.order_status_id', 'desc')
+                                ->orderBy('orders.id', 'asc')
+                                ->select('orders.*');
+            }
+        } else {
+            if ($isManager) {
+                return $model->whereIn('orders.order_status_id', [2,3,4])
+                                ->orderBy('orders.order_status_id', 'desc')
+                                ->orderBy('orders.id', 'asc')
+                                ->select('orders.*');
+
+            } else {
+                return $model->where([
+                                    ['orders.driver_id', '=', $this->request->get('driver_id')],
+                                    ['orders.order_status_id', '<>', 5],
+                                    ['orders.order_status_id', '<>', 1],
+                                ])
+                                ->orderBy('orders.order_status_id', 'desc')
+                                ->orderBy('orders.id', 'asc')
+                                ->select('orders.*');
+            }
+        }
+
+
+
+
 
     }
 }
